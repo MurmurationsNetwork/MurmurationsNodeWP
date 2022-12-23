b7ceeb9a2f79220179737e5c9c1baa8feb99d8df
Django Doucet <mediaformat.ux@gmail.com>
2022-12-23 12:35:24 -0700

    version increment

--- murmurations-node.php
+++ murmurations-node.php
@@ -7,7 +7,7 @@
  * Author URI:      https://murmurations.network
  * Text Domain:     murmurations-node
  * Domain Path:     /languages
- * Version:         0.2.0
+ * Version:         0.2.2
  *
  * @package         Murmurations_Node
  */
