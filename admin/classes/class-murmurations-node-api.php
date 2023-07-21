09e9e2f1051882a8dce0e84cb91a769b3728189e
TingSyuanWang <vincent@fishboneapps.com>
2023-07-21 15:12:50 -0400

    feat: add DELETE single profile
    Signed-off-by: TingSyuanWang <vincent@fishboneapps.com>

--- admin/classes/class-murmurations-node-api.php
+++ admin/classes/class-murmurations-node-api.php
@@ -45,6 +45,14 @@ if ( ! class_exists( 'Murmurations_Node_API' ) ) {
 	            );
 
 	            // delete a profile
+	            register_rest_route(
+					'murmurations-node/v1',
+		            '/profile/(?P<cuid>[\w]+)',
+		            array(
+						'methods'  => 'DELETE',
+						'callback' => array( $this, 'delete_profile' ),
+                    )
+	            );
             });
         }
 
@@ -189,6 +197,37 @@ if ( ! class_exists( 'Murmurations_Node_API' ) ) {
 
 			$response = array(
 				'message' => esc_html__('Profile updated successfully.', 'text-domain'),
+			);
+
+			return rest_ensure_response($response);
+		}
+
+		public function delete_profile( $request ) {
+			global $wpdb;
+			$table_name = $wpdb->prefix . 'murmurations_profiles';
+
+			$cuid = $request['cuid'];
+
+			$existing_profile = $wpdb->get_row(
+				$wpdb->prepare("SELECT * FROM $table_name WHERE cuid = %s", $cuid),
+				ARRAY_A
+			);
+
+			if (empty($existing_profile)) {
+				return new WP_Error('profile_not_found', esc_html__('Profile not found.', 'text-domain'), array('status' => 404));
+			}
+
+			$delete_result = $wpdb->delete(
+				$table_name,
+				array('cuid' => $cuid)
+			);
+
+			if ($delete_result === false) {
+				return new WP_Error('delete_failed', esc_html__('Failed to delete the profile.', 'text-domain'), array('status' => 500));
+			}
+
+			$response = array(
+				'message' => esc_html__('Profile deleted successfully.', 'text-domain'),
 			);
 
 			return rest_ensure_response($response);
