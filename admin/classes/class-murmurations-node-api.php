c5233cd2c4aa889d4a1fd937fc0f66da37d06935
TingSyuanWang <vincent@fishboneapps.com>
2023-07-21 14:01:25 -0400

    feat: add GET profiles api
    Signed-off-by: TingSyuanWang <vincent@fishboneapps.com>

--- admin/classes/class-murmurations-node-api.php
+++ admin/classes/class-murmurations-node-api.php
@@ -3,20 +3,47 @@
 if ( ! class_exists( 'Murmurations_Node_API' ) ) {
     class Murmurations_Node_API {
         public function __construct() {
-            add_action( 'wp_ajax_react-wp-admin-ajax', array( $this, 'handle_ajax' ) );
+            add_action( 'rest_api_init', function () {
+				register_rest_route(
+					'murmurations-node/v1',
+					'/get_profiles',
+					array(
+						'methods'  => 'GET',
+						'callback' => array( $this, 'get_profiles' ),
+					)
+				);
+            });
         }
 
-        public function handle_ajax() {
-            header( 'Content-Type: application/json' );
+        public function get_profiles( $request ) {
+//			// todo: we might need to set the permission, it's not working now.
+//			if ( ! current_user_can( 'read') ) {
+//				return new WP_Error( 'rest_forbidden', esc_html__( 'You cannot view the profiles.' ), array( 'status' => 401 ) );
+//			}
 
-            $user = wp_get_current_user();
+	        global $wpdb;
+	        $table_name = $wpdb->prefix . 'murmurations_profiles';
 
-            echo json_encode( array(
-                'name'    => $user ? $user->data->display_name : 'there',
-                'message' => 'API call works!',
-            ) );
+	        $profiles = $wpdb->get_results( "SELECT * FROM $table_name", ARRAY_A );
 
-            exit;
+	        if ( empty( $profiles ) ) {
+		        return new WP_Error( 'no_profiles_found', esc_html__( 'No profiles found.', 'text-domain' ), array( 'status' => 404 ) );
+	        }
+
+	        $data = array();
+	        foreach ( $profiles as $profile ) {
+		        $data[] = array(
+			        'id'            => $profile['id'],
+			        'cuid'          => $profile['cuid'],
+			        'node_id'       => $profile['node_id'],
+			        'linked_schemas' => json_decode( $profile['linked_schemas'], true ),
+			        'profile'       => json_decode( $profile['profile'], true ),
+			        'created_at'    => $profile['created_at'],
+			        'updated_at'    => $profile['updated_at'],
+		        );
+	        }
+
+            return rest_ensure_response( $data );
         }
     }
 }
