66a86686a98a38379282856b9bde001468ab4a99
TingSyuanWang <vincent@fishboneapps.com>
2023-07-21 14:44:58 -0400

    feat: add GET single profile
    Signed-off-by: TingSyuanWang <vincent@fishboneapps.com>

--- admin/classes/class-murmurations-node-api.php
+++ admin/classes/class-murmurations-node-api.php
@@ -4,6 +4,7 @@ if ( ! class_exists( 'Murmurations_Node_API' ) ) {
     class Murmurations_Node_API {
         public function __construct() {
             add_action( 'rest_api_init', function () {
+				// get all profiles
 				register_rest_route(
 					'murmurations-node/v1',
 					'/get_profiles',
@@ -12,6 +13,22 @@ if ( ! class_exists( 'Murmurations_Node_API' ) ) {
 						'callback' => array( $this, 'get_profiles' ),
 					)
 				);
+
+				// get a profile
+				register_rest_route(
+					'murmurations-node/v1',
+					'/get_profile/(?P<cuid>[\w]+)',
+	                array(
+						'method'    => 'GET',
+                        'callback'  => array( $this, 'get_profile' ),
+	                )
+				);
+
+	            // post a profile
+
+	            // edit a profile
+
+	            // delete a profile
             });
         }
 
@@ -45,5 +62,34 @@ if ( ! class_exists( 'Murmurations_Node_API' ) ) {
 
             return rest_ensure_response( $data );
         }
+
+		public function get_profile( $request ) {
+			global $wpdb;
+			$table_name = $wpdb->prefix . 'murmurations_profiles';
+
+			$cuid = $request['cuid'];
+
+			$profile = $wpdb->get_row(
+				$wpdb->prepare("SELECT * FROM $table_name WHERE cuid = %s", $cuid),
+				ARRAY_A
+			);
+
+			if (empty($profile)) {
+				return new WP_Error('profile_not_found', esc_html__('Profile not found.', 'text-domain'), array('status' => 404));
+			}
+
+			// Prepare the data to be returned in the API response
+			$data = array(
+				'id' => $profile['id'],
+				'cuid' => $profile['cuid'],
+				'node_id' => $profile['node_id'],
+				'linked_schemas' => json_decode($profile['linked_schemas'], true),
+				'profile' => json_decode($profile['profile'], true),
+				'created_at' => $profile['created_at'],
+				'updated_at' => $profile['updated_at'],
+			);
+
+			return rest_ensure_response($data);
+		}
     }
 }
