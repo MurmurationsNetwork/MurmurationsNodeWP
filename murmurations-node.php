0ed2c8b27a72b91fd0345673ecb55e32d183a854
TingSyuanWang <vincent@fishboneapps.com>
2023-11-17 23:18:47 -0500

    chore: bump the version
    Signed-off-by: TingSyuanWang <vincent@fishboneapps.com>

--- murmurations-node.php
+++ murmurations-node.php
@@ -3,7 +3,7 @@
  * Plugin Name:       Murmurations Profile Generator
  * Plugin URI:        https://github.com/MurmurationsNetwork/MurmurationsNodeWP
  * Description:       Add your profile to the Murmurations distributed data sharing network.
- * Version:           1.0.0-beta.6
+ * Version:           1.0.0-beta.7
  * Requires at least: 6.4
  * Text Domain:       murmurations-node
  * Author:            Murmurations Network
@@ -33,51 +33,51 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 if ( ! class_exists( 'MurmurationsNode' ) ) {
-    define( 'MURMURATIONS_NODE_URL', plugin_dir_url( __FILE__ ) );
-    define( 'MURMURATIONS_NODE_DIR', __DIR__ );
-	define( 'MURMURATIONS_NODE_TABLE', 'murmurations_profiles');
+	define( 'MURMURATIONS_NODE_URL', plugin_dir_url( __FILE__ ) );
+	define( 'MURMURATIONS_NODE_DIR', __DIR__ );
+	define( 'MURMURATIONS_NODE_TABLE', 'murmurations_profiles' );
 
-    class MurmurationsNode {
-        public function __construct() {
-            $this->register_autoloads();
-            $this->register_admin_page();
-            $this->register_api();
+	class MurmurationsNode {
+		public function __construct() {
+			$this->register_autoloads();
+			$this->register_admin_page();
+			$this->register_api();
 			$this->register_upgrade();
-        }
+		}
 
-        private function register_autoloads() {
-            spl_autoload_register(function($name){
-				$name = strtolower($name);
-				$name = str_replace('_', '-', $name);
+		private function register_autoloads() {
+			spl_autoload_register( function ( $name ) {
+				$name = strtolower( $name );
+				$name = str_replace( '_', '-', $name );
 				$name = 'class-' . $name;
 				$file = __DIR__ . '/admin/classes/' . $name . '.php';
 
 				if ( file_exists( $file ) ) {
 					require_once $file;
 				}
-			});
-        }
+			} );
+		}
 
-        public function register_admin_page() {
-            new Murmurations_Node_Admin_Page();
-        }
+		public function register_admin_page() {
+			new Murmurations_Node_Admin_Page();
+		}
 
-        public function register_api() {
-            new Murmurations_Node_API();
-        }
+		public function register_api() {
+			new Murmurations_Node_API();
+		}
 
 		public function register_upgrade() {
 			new Murmurations_Node_Upgrade();
 		}
-    }
+	}
 
-    new MurmurationsNode();
+	new MurmurationsNode();
 }
 
-if ( class_exists('Murmurations_Node_Activation') ) {
-	register_activation_hook( __FILE__, array('Murmurations_Node_Activation', 'activate' ) );
+if ( class_exists( 'Murmurations_Node_Activation' ) ) {
+	register_activation_hook( __FILE__, array( 'Murmurations_Node_Activation', 'activate' ) );
 }
 
-if ( class_exists('Murmurations_Node_Uninstall') ) {
-	register_uninstall_hook( __FILE__, array('Murmurations_Node_Uninstall', 'uninstall' ));
+if ( class_exists( 'Murmurations_Node_Uninstall' ) ) {
+	register_uninstall_hook( __FILE__, array( 'Murmurations_Node_Uninstall', 'uninstall' ) );
 }
